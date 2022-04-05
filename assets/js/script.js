4a684d001306311daf13e3af9658ecd89da81f5e
Josiah-Canada <josiahcanada@yahoo.com>
2022-04-05 11:05:17 -0400

    make list items stay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
@@ -8,4 +8,17 @@ var createTaskHandler = function() {
     tasksToDoEl.appendChild(listItemEl);
   }
 
-  buttonEl.addEventListener("click", createTaskHandler);
+  var createTaskHandler = function(event) {
+
+    event.preventDefault();
+  
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  };
+  
+
+  formEl.addEventListener("submit", createTaskHandler);
+
+ 
